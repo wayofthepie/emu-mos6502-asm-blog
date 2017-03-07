f1162f429bf3a72677eef26da6f1536acc69bf97
wayofthepie <wayofthepie@gmail.com>
2017-03-07 21:24:37 +0000

    Add a test to check consumptoin for byte
    byte should not consume more chars if it is successful, add a test to ensure this.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# emu-mos6502-asm-blog
-Code for the blog post on building a 6502 assembler in haskell.
+# What is this?
+This repo contains the code for the blog post on building a 6502 assembler in haskell.
+
+# Building
+Install [stack](https://docs.haskellstack.org/en/stable/README/#how-to-install) and simply run `stack
+install` to build.
--- test/AssemblerSpec.hs
+++ test/AssemblerSpec.hs
@@ -7,18 +7,24 @@ import Text.Megaparsec
 import Assembler
 
 import Test.Hspec.Megaparsec
+import Test.QuickCheck.Instances
 import Test.Tasty.Hspec
 import Test.Tasty.HUnit
 import Test.Tasty.QuickCheck as QC
-
+import Debug.Trace
 spec :: Spec
 spec = asmSpec
 
 asmSpec = do
-  describe "byte" $
-    it "should parse two consecutive characters in the hex range into a two character string" $
-      property prop_byte_parse
+  describe "byte" $ do
+    it "should parse two consecutive characters in the hex range into a two character string" $ do
+      property prop_byte_parseValidData
+    it "should parse two chars and leave the rest of the string unconsumed, if successful" $ do
+      property prop_byte_parseSuccessShouldNotConsume
 
+--------------------------------------------------------------------------------
+-- byte
+--------------------------------------------------------------------------------
 newtype TwoCharHexString = TwoCharHexString T.Text deriving Show
 
 instance Arbitrary TwoCharHexString where
@@ -31,4 +37,12 @@ instance Arbitrary TwoCharHexString where
     y <- elements vals
     pure $ TwoCharHexString (T.pack (x:[y]))
 
-prop_byte_parse (TwoCharHexString s) = parse byte "" s  `shouldParse` s
+newtype ValidFirstTwoChars = ValidFirstTwoChars T.Text deriving Show
+
+-- Shoud parse valid two char hexstring.
+prop_byte_parseValidData (TwoCharHexString s) = parse byte "" s  `shouldParse` s
+
+-- When successful should not consume more input.
+prop_byte_parseSuccessShouldNotConsume (TwoCharHexString s) extra =
+  runParser' byte (initialState (T.append s extra)) `succeedsLeaving` extra
+
